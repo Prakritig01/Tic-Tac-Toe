47d3c7f5a7d0ddf9a4af883a651a8bfb6e3c479e
Prakritig01 <prakritigupta1112002@gmail.com>
2024-11-07 22:18:38 +0530

    tic-tac-toe

---
+++ script.js
@@ -0,0 +1,57 @@
+let boxes = document.querySelectorAll(".box");
+
+let turn0 = true;
+const winPatterns = [
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8],
+];
+
+boxes.forEach((box) => {
+    box.addEventListener("click" , ()=>{
+        console.log("block was clicked");
+        if(turn0){
+            box.innerHTML = "O";
+            turn0 = false;
+
+        }
+        else{
+            box.innerHTML = "X";
+            turn0 = true;
+        }
+        box.disabled = true;
+        checkWinner();
+    });
+});
+
+function checkWinner(){
+    for(let pattern of winPatterns){
+        
+        let pos1Val = boxes[pattern[0]].innerHTML;
+        let pos2Val = boxes[pattern[1]].innerHTML;
+        let pos3Val = boxes[pattern[2]].innerHTML;
+
+        if(pos1Val != "" && pos2Val != "" && pos3Val != ""){
+            if(pos1Val === pos2Val && pos2Val === pos3Val) {
+                setTimeout(() => {
+                    boxes[pattern[0]].classList.add("winner");
+                    boxes[pattern[1]].classList.add("winner");
+                    boxes[pattern[2]].classList.add("winner");
+                    disableAllBoxes();
+                }, 10);
+                break;
+            }
+        }
+    }
+}
+
+function disableAllBoxes(){
+    boxes.forEach((box) =>{
+        box.disabled = true;
+    })
+}
